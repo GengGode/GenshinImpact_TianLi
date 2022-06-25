8fcedc17fc31ce5a96b63002be24543da53b4108
Gode King <GengGode@outlook.com>
2022-06-25 23:19:56 +0800

    Resource测试

---
+++ Test_GenshinImpact_TianLi_Resource/test.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+#include "..\GenshinImpact_TianLi_Resource\GenshinImpact_TianLi_Resource.h"
+#pragma comment(lib,"GenshinImpact_TianLi_Resource.lib")
+
+TEST(TestCaseName, TestName) {
+	HBITMAP map = LoadBitmap_GIMAP();
+	EXPECT_NE(map, nullptr);
+}
