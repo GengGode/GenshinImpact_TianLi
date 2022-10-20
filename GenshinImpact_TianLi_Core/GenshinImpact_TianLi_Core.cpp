ed6fcc9420f6859b9b97bcbe5f09487286f973f1
GengGode <GengGode@outlook.com>
2022-10-21 05:50:43 +0800

    feat.Core中添加Map

--- GenshinImpact_TianLi_Core/GenshinImpact_TianLi_Core.cpp
+++ GenshinImpact_TianLi_Core/GenshinImpact_TianLi_Core.cpp
@@ -1,27 +1,33 @@
 #include "pch.h"
 #include "GenshinImpact_TianLi_Core.h"
 
-#include "..\GenshinImpact_TianLi_Sqlite\GenshinImpact_TianLi_Sqlite.h"
-#pragma comment(lib,"GenshinImpact_TianLi_Sqlite.lib")
-
 #include "..\GenshinImpact_TianLi_Resource\GenshinImpact_TianLi_Resource.h"
 #pragma comment(lib,"GenshinImpact_TianLi_Resource.lib")
+
+#include "..\GenshinImpact_TianLi_Sqlite\GenshinImpact_TianLi_Sqlite.h"
+#pragma comment(lib,"GenshinImpact_TianLi_Sqlite.lib")
 
 #include "..\GenshinImpact_TianLi_Track\GenshinImpact_TianLi_Track.h"
 #pragma comment(lib,"GenshinImpact_TianLi_Track.lib")
 
+#include "..\GenshinImpact_TianLi_Map\GenshinImpact_TianLi_Map.h"
+#pragma comment(lib,"GenshinImpact_TianLi_Map.lib")
+
 GenshinImpact_TianLi_Core::GenshinImpact_TianLi_Core()
 {
-	sqlite = new GenshinImpact_TianLi_Sqlite();
 	resource = GenshinImpact_TianLi_Resource::GetInstance();
+	
+	sqlite = new GenshinImpact_TianLi_Sqlite();
 	track = new GenshinImpact_TianLi_Track();
+	map = new GenshinImpact_TianLi_Map();
 }
 
 GenshinImpact_TianLi_Core::~GenshinImpact_TianLi_Core()
 {
-	delete sqlite;
 	delete resource;
+	delete sqlite;
 	delete track;
+	delete map;
 }
 
 GenshinImpact_TianLi_Core& GenshinImpact_TianLi_Core::GetInstance()
--- GenshinImpact_TianLi_Core/GenshinImpact_TianLi_Core.h
+++ GenshinImpact_TianLi_Core/GenshinImpact_TianLi_Core.h
@@ -18,33 +18,40 @@
 #endif // DLLAPI
 #endif
 
-#include "..\GenshinImpact_TianLi_Sqlite\GenshinImpact_TianLi_Sqlite.h"
-#pragma comment(lib,"GenshinImpact_TianLi_Sqlite.lib")
-
 #include "..\GenshinImpact_TianLi_Resource\GenshinImpact_TianLi_Resource.h"
 #pragma comment(lib,"GenshinImpact_TianLi_Resource.lib")
+
+#include "..\GenshinImpact_TianLi_Sqlite\GenshinImpact_TianLi_Sqlite.h"
+#pragma comment(lib,"GenshinImpact_TianLi_Sqlite.lib")
 
 #include "..\GenshinImpact_TianLi_Track\GenshinImpact_TianLi_Track.h"
 #pragma comment(lib,"GenshinImpact_TianLi_Track.lib")
 
+#include "..\GenshinImpact_TianLi_Map\GenshinImpact_TianLi_Map.h"
+#pragma comment(lib,"GenshinImpact_TianLi_Map.lib")
+
 class GenshinImpact_TianLi_Sqlite;
 class GenshinImpact_TianLi_Resource;
 class GenshinImpact_TianLi_Track;
+class GenshinImpact_TianLi_Map;
 
 class DLLAPI GenshinImpact_TianLi_Core
 {
-	GenshinImpact_TianLi_Sqlite* sqlite;
 	GenshinImpact_TianLi_Resource* resource;
+	GenshinImpact_TianLi_Sqlite* sqlite;
 	GenshinImpact_TianLi_Track* track;
+	GenshinImpact_TianLi_Map* map;
 	
 	GenshinImpact_TianLi_Core();
 public:
 	~GenshinImpact_TianLi_Core();
 	static GenshinImpact_TianLi_Core& GetInstance();
 public:
-	GenshinImpact_TianLi_Sqlite& GetSqlite() { return *sqlite; }
 	GenshinImpact_TianLi_Resource& GetResource() { return *resource; }
+	GenshinImpact_TianLi_Sqlite& GetSqlite() { return *sqlite; }
 	GenshinImpact_TianLi_Track& GetTrack() { return *track; }
+	GenshinImpact_TianLi_Map& GetMap() { return *map; }
+	
 	
 };
 
