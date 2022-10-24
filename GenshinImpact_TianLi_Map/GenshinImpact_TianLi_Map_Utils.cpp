5da36b2ccaf4f9ca7d84cbd88d2e04fa6934bcf4
GengGode <GengGode@outlook.com>
2022-10-24 22:30:54 +0800

    fix.修复绘制叠加透明图片的错误效果

--- GenshinImpact_TianLi_Map/GenshinImpact_TianLi_Map_Utils.cpp
+++ GenshinImpact_TianLi_Map/GenshinImpact_TianLi_Map_Utils.cpp
@@ -19,26 +19,21 @@ void TianLi::Map::Utils::add_rgba_image(const cv::Mat& src1, const cv::Mat& src2
 	//assert(src1.size() != src2.size());
 	assert(src1.channels() == 4 && src2.channels() == 4);
 	
-	// 根据透明通道叠加 Color = Src1.rgb * Src1.a + Src2.rgb * Src2.a * alpha
-	
 	std::vector<cv::Mat> src1_split;
 	std::vector<cv::Mat> src2_split;
 	std::vector<cv::Mat> dst_merge;
 
 	cv::split(src1, src1_split);
 	cv::split(src2, src2_split);
-	
+
 	for (int i = 0; i < 3; i++)
 	{
-		cv::Mat dst_src1 = src1_split[i].mul(src1_split[3], 1.0 / 255.0);
-		cv::Mat dst_src2 = src2_split[i].mul(src2_split[3], alpha / 255.0);
-		cv::Mat dst_channel = dst_src1 + dst_src2;
+		auto dst_src1 = src1_split[i].mul(~src2_split[3], 1.0 / 255.0);
+		auto dst_src2 = src2_split[i].mul(src2_split[3], alpha / 255.0);
+		auto dst_channel = dst_src1 + dst_src2;
 		dst_merge.push_back(dst_channel);
 	}
-
 	cv::Mat alpha_dst = src1_split[3] + src2_split[3] * alpha;
-	// cv::scaleAdd(src2_split[3], alpha, src1_split[3], alpha_dst);
 	dst_merge.push_back(alpha_dst);
-	
 	cv::merge(dst_merge, dst);
 }
