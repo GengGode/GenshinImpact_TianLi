259251e8d1ab327618e6c1f0ce37f8d0201a05f4
GengGode <GengGode@outlook.com>
2022-11-15 04:41:16 +0800

    feat:SelectedItemButton按钮添加设置进度接口

--- GenshinImpact_TianLi/TianLiQtCommon_SelectedItemButton.h
+++ GenshinImpact_TianLi/TianLiQtCommon_SelectedItemButton.h
@@ -24,12 +24,15 @@ private:
 	// UI÷ÿªÊ
 private:
 	//void paintEvent(QPaintEvent* event) Q_DECL_OVERRIDE;
-	
+	int progress_max_number = 100;
+	int progress_count = 5;
 public:
 	void setTitle(QString title);
 	void setRegion(QString region);
 	void setImage(QImage image);
 	void setLabelImage(QImage image);
+	void setProgressCount(int number);
+	void setProgressMaxNumber(int number);
 signals:
 	void signal_double_click(bool is_checked);
 };
